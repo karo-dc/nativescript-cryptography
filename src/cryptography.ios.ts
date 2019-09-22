56b8c1ce6a87478ab6818204e376f946b3c13218
Karo Ghazaryan <karo.ghazarian@gmail.com>
2019-09-22 17:31:48 +0400

    feat: main functonality

---
+++ src/cryptography.ios.ts
@@ -0,0 +1,5 @@
+import { Common } from './cryptography.common';
+
+export class Cryptography extends Common {
+
+}
---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+import { Common } from './cryptography.common';
+export declare class Cryptography extends Common {
+  // define your typings manually
+  // or..
+  // take the ios or android .d.ts files and copy/paste them here
+}
